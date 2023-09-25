d2c5fb5552deb981b35594ac22bceb38f65fd8d5
LiviuDragutan <liviu_dragutan@yahoo.com>
2023-09-25 15:26:41 +0300

    messag1

---
+++ index.js
@@ -0,0 +1,70 @@
+
+const userName = 'Liviu';
+const last_name = 'Dragutan'
+const test = 'Uppercase'; 
+
+
+const str1 = 'I'
+const str2 = 'Love'
+const str3 = 'Coding'
+
+console.log (`${str1} ${str2}  ${str3}  123456789 ${str3}`)
+
+const numeero = 4
+console.log(typeof numeero.toString())
+const strin1 = '21'
+console.log( Boolean(21))
+const strin2 = '22'
+console.log(typeof Number(strin2))
+const strin3 = '24'
+console.log(typeof String(strin3))
+
+
+const condition = true;
+const falsy1 = 0;
+const falsy2 = '';
+const falsy3 = undefined;
+const falsy4 =  null;
+const falsy5 = NaN;
+const falsy6 = -0;
+
+ if(falsy1) {
+    console.log('is ok')
+ } else {
+    console.log('is not ok')
+ }
+ 
+ if(falsy2) {
+    console.log('is ok')
+ } else {
+    console.log('is not ok')
+ }
+ 
+ if(falsy3) {
+    console.log('is ok')
+ } else {
+    console.log('is not ok')
+ }
+ 
+ if(falsy4) {
+    console.log('is ok')
+ } else {
+    console.log('is not ok')
+ }
+ 
+ if(falsy5) {
+    console.log('is ok')
+ } else {
+    console.log('is not ok')
+ }
+ 
+ if(falsy6) {
+    console.log('is ok')
+ } else {
+    console.log('is not ok')
+ }
+
+
+
+ console.log (1===2 && 2==2);
+ console.log (1===1||2==22);
